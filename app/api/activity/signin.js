09c5bc5cad388129e46657c46f879b8b43cc9d6a
ken <ckken@qq.com>
2016-01-26 11:20:24 +0800

    完善 restful 使用方法 增加签到会员显示

--- app/api/activity/signin.js
+++ app/api/activity/signin.js
@@ -6,30 +6,26 @@ export default class extends G.controller.rest {
         this.modelName = 'activity_signin'
     }
 
-    asyn
 
     async get(){
         var rs = await super.get({cb:true})
-        //console.log(rs)
-        /*_.forEach(rs.data.pageData,async(v,k)=>{
-
-            rs.data.pageData[k].count = await D.model('activity_signin_member').count({aid:v.id}).toPromise()
-            rs.data.pageData[k].online = await D.model('activity_signin_member').count({aid:v.id,online:1}).toPromise()
-            console.log(rs.data.pageData[k])
-        })*/
-
-        /*await rs.data.pageData.forEach(async (v,k)=>{
-            v.count = await D.model('activity_signin_member').count({aid:v.id}).toPromise()
-            v.online = await D.model('activity_signin_member').count({aid:v.id,online:1}).toPromise()
-        })*/
 
         for(let i=0;i<rs.data.pageData.length;i++){
             rs.data.pageData[i].count = await D.model('activity_signin_member').count({aid:rs.data.pageData[i].id}).toPromise()
             rs.data.pageData[i].online = await D.model('activity_signin_member').count({aid:rs.data.pageData[i].id,online:1}).toPromise()
         }
+        this.json(rs)
+    }
 
-
-
+    async members(){
+        let rs = {}
+        this._map.aid = this.req.params.id
+        if(this._map.aid){
+            this.modelName = 'activity_signin_member'
+            rs = await super.get({cb:true})
+            console.log(this._map)
+            console.log(rs)
+        }
         this.json(rs)
     }
 

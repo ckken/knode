bef252bd4454f38c2bbe75a7efc0b61356398caa
ken <ckken@qq.com>
2016-02-17 14:40:40 +0800

    分离统计模块

---
+++ payCount/config/database.js
@@ -0,0 +1,32 @@
+export default {
+    db: {
+        connections: {
+/*            local: {
+                adapter: 'mongo',
+                host: '172.16.8.213',
+                port: 27017,
+                user: '',
+                password: '',
+                database: 'yolipai'
+            },*/
+            local: {
+                adapter: 'mongo',
+                host: '127.0.0.1',
+                port: 27017,
+                user: '',
+                password: '',
+                database: 'yolipai'
+            },
+
+            /*erp: {
+                adapter: 'sqlserver',
+                host: 'em.it250.cn',
+                port: 8952,
+                user: 'ylp',
+                password: 'ylp.2015',
+                database: 'WXD_EM'
+            }*/
+
+        }
+    }
+}

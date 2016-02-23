add8d72cdda4e968b43025e69365e6b6c4137916
ken <ckken@qq.com>
2016-02-24 02:40:43 +0800

    修复活动次数问题

--- application/ylp-activities/socket/activity_redpack_analysis.js
+++ application/ylp-activities/socket/activity_redpack_analysis.js
@@ -127,7 +127,7 @@ module.exports = (io) => {
                     analysis.playMember = await member_mod.count({aid: socket.roomId}).toPromise()
                     //赋值
                     cache[socket.roomId].analysis = analysis
-                }else{
+                }else if(cache[socket.roomId].analysis){
                     //补全已经参与人员数据
                     cache[socket.roomId].analysis.playMember = await member_mod.count({aid: socket.roomId}).toPromise()
                 }

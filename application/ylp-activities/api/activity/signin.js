de3a5e81ed5032e97cd952677452485a0ca55657
Abel <348720167@qq.com>
2016-03-02 21:14:16 +0800

    签到活动单点禁言

--- application/ylp-activities/api/activity/signin.js
+++ application/ylp-activities/api/activity/signin.js
@@ -31,7 +31,7 @@ export default class extends base {
             this.modelName = 'activity_signin_member'
             if(this.req.query.nickname){
                 console.log(1111)
-                rs = await super.get({polulate:{select:this.req.query.nickname},cb:true})
+                rs = await super.get({polulate:{model:this.modelName,select:this.req.query.nickname},cb:true})
             }else{
                 rs = await super.get({cb:true})
             }

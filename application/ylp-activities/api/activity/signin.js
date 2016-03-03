f501f7e55d52af11d5dc0988a90e62f4ca741326
leeyangyi <839273739@qq.com>
2016-03-03 11:44:14 +0800

    剩余红包数

--- application/ylp-activities/api/activity/signin.js
+++ application/ylp-activities/api/activity/signin.js
@@ -27,7 +27,7 @@ export default class extends base {
         delete this._map.ylpEnterpriseAccountNo
         delete this._map.userAccountNo
         this._map.aid = this.req.params.id
-        if(this.req.query.nickname)this._map.nickname = '/'+this.req.query.nickname+'/'
+        if(this.req.query.nickname)this._map.nickname = {'like': '%'+ this.req.query.nickname +'%' }
         console.log(this._map)
         if(this._map.aid){
             this.modelName = 'activity_signin_member'

6aaee83a38310c0e21c59e288372d73ed989aaa6
ken <ckken@qq.com>
2016-03-04 23:14:17 +0800

    重构环境变量

--- application/ylp-analysis-mq/model/ylp_analysis/analysis_pay.js
+++ application/ylp-analysis-mq/model/ylp_analysis/analysis_pay.js
@@ -1,9 +1,25 @@
 module.exports = {
     //schema:true,
     attributes: {
-        "paySuccessTime":{type:'string'},
-        "completeTime":{type:'string'},
-        "createTime":{type:'string'},
-        "modifyTime":{type:'string'}
-    }
+        /*"paySuccessTime":{type:'string'},
+         "completeTime":{type:'string'},
+         "createTime":{type:'string'},
+         "modifyTime":{type:'string'}*/
+    },
+    //
+    beforeCreate: function (values, cb) {
+        values.createTime = _.moment(values.createTime).format('YYYY-MM-DD HH:mm:ss')
+        values.modifyTime = _.moment(values.modifyTime).format('YYYY-MM-DD HH:mm:ss')
+        values.paySuccessTime = _.moment(values.paySuccessTime).format('YYYY-MM-DD HH:mm:ss')
+        values.completeTime = _.moment(values.completeTime).format('YYYY-MM-DD HH:mm:ss')
+        cb();
+    },
+    //
+    beforeUpdate: function (values, cb) {
+        values.createTime = _.moment(values.createTime).format('YYYY-MM-DD HH:mm:ss')
+        values.modifyTime = _.moment(values.modifyTime).format('YYYY-MM-DD HH:mm:ss')
+        values.paySuccessTime = _.moment(values.paySuccessTime).format('YYYY-MM-DD HH:mm:ss')
+        values.completeTime = _.moment(values.completeTime).format('YYYY-MM-DD HH:mm:ss')
+        cb();
+    },
 }

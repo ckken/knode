a09cd057a95000a8c4a30e46760298c7dc84ea91
ken <ckken@qq.com>
2016-03-04 22:48:45 +0800

    重构环境变量

--- application/ylp-analysis-mq/socket/payCountMQ.js
+++ application/ylp-analysis-mq/socket/payCountMQ.js
@@ -5,25 +5,25 @@ import Stomp from 'stompjs';
 let model = D.model('analysis_pay')
 if (G.mq && G.mq_pay_queue) {
     let client = Stomp.overTCP(G.mq.host, 61613)
-    client.connect(G.mq.username, G.mq.password, async(d)=>{
+    client.connect(G.mq.username, G.mq.password, async(d)=> {
+        //console.log('model data',await model.find().limit(10).sort({id:'desc'}).toPromise())
         //console.log(d)
-        client.subscribe(G.mq_pay_queue, async(d)=>{
+        client.subscribe(G.mq_pay_queue, async(d)=> {
             //
             let data = JSON.parse(d.body)
             let checkCount = await model.count({bankOrderNo: data.bankOrderNo}).toPromise()
             //
-            console.log('==='+G.mq_pay_queue+'===' + Date.now() + '====', data.bankOrderNo+'====mongoCount=='+checkCount);
+            console.log('===' + G.mq_pay_queue + '===' + Date.now() + '====', data.bankOrderNo + '====mongoCount==' + checkCount);
             if (checkCount > 0) {
                 await model.update({bankOrderNo: data.bankOrderNo}, data).toPromise()
             } else {
                 await model.create(data).toPromise()
             }
         })
-    }, (d)=>{
+    }, (d)=> {
         return console.log(d)
     })
 }
 module.exports = async (io) => {
 
-    console.log(await model.find().limit(10).toPromise())
 }

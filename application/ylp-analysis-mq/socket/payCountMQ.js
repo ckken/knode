70c520967d5eb4df6c768a129b20fdbd0941a5eb
ken <ckken@qq.com>
2016-03-04 22:51:08 +0800

    重构环境变量

--- application/ylp-analysis-mq/socket/payCountMQ.js
+++ application/ylp-analysis-mq/socket/payCountMQ.js
@@ -7,9 +7,9 @@ if (G.mq && G.mq_pay_queue) {
     let client = Stomp.overTCP(G.mq.host, 61613)
     client.connect(G.mq.username, G.mq.password, async(d)=> {
         //console.log('model data',await model.find().limit(10).sort({id:'desc'}).toPromise())
-        console.log(d)
         client.subscribe(G.mq_pay_queue, async(d)=> {
             //
+            console.log(d)
             let data = JSON.parse(d.body)
             let checkCount = await model.count({bankOrderNo: data.bankOrderNo}).toPromise()
             //

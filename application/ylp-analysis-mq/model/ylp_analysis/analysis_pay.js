be25e394178fa11a83da34efacf69de7e5c47caf
ken <ckken@qq.com>
2016-03-05 21:39:00 +0800

    重构环境变量

--- application/ylp-analysis-mq/model/ylp_analysis/analysis_pay.js
+++ application/ylp-analysis-mq/model/ylp_analysis/analysis_pay.js
@@ -8,20 +8,20 @@ module.exports = {
     },
     //
     beforeCreate: function (values, cb) {
-        console.log('beforeCreate',values)
         values.createTime = _.moment(values.createTime).format('YYYY-MM-DD HH:mm:ss')
         values.modifyTime = _.moment(values.modifyTime).format('YYYY-MM-DD HH:mm:ss')
         values.paySuccessTime = _.moment(values.paySuccessTime).format('YYYY-MM-DD HH:mm:ss')
         values.completeTime = _.moment(values.completeTime).format('YYYY-MM-DD HH:mm:ss')
+        console.log('beforeCreate',JSON.stringify(values))
         cb();
     },
     //
     beforeUpdate: function (values, cb) {
-        console.log('beforeUpdate',values)
         values.createTime = _.moment(values.createTime).format('YYYY-MM-DD HH:mm:ss')
         values.modifyTime = _.moment(values.modifyTime).format('YYYY-MM-DD HH:mm:ss')
         values.paySuccessTime = _.moment(values.paySuccessTime).format('YYYY-MM-DD HH:mm:ss')
         values.completeTime = _.moment(values.completeTime).format('YYYY-MM-DD HH:mm:ss')
+        console.log('beforeUpdate',JSON.stringify(values))
         cb();
     },
 }

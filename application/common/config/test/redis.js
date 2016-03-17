811f1d47ce25eb8e5d4d78f83f470d8682783600
leeyangyi <839273739@qq.com>
2016-03-17 12:07:43 +0800

    默认值

--- application/common/config/test/redis.js
+++ application/common/config/test/redis.js
@@ -1,8 +1,8 @@
 export default {
     redis:{
-        host:'172.16.8.161',
+        host:'172.16.8.164',
         username:'root',
         password:'yolipai168',
-        port:'7002'
+        port:'7004'
     }
 }
